8c5eca6a70c562e6f1d4f27c680e93236474929c
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-16 22:46:26 +0000

    Added mouse wasd movements

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,10 +11,18 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // your code here
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
